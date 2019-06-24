6c6628c377d20fb201adc45795c87265d38a41bb
stas-job4j <saydobenothing@gmail.com>
2019-06-24 21:23:50 +0300

    1.3. JavaDoc. Code convention[#152397]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+*Class Calculate . Решение задачи 1.3. JavaDoc. Code convention[#152397]
+*@author Stanislau S. (saydobenothing@gmail.com)
+*@since 24.06.2018
+*/
 public class Calculate {   
-   public static void main(String[] args) {
+ 
+/**
+* Main . Точка входа в программу .
+* @param args - args (метод имеет параметр args)
+*/
+  public static void main(String[] args)
+  {
       System.out.println("Hello, Job4j.");
    }
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Stanislau S. (saydobenothing@gmail.com)
+* @version 1
+* @since 24.06.2018
+*/
+package ru.job4j.calculate;
