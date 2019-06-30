f4469f31117d8346f6a04284766ff41767cbbaa1
stas-job4j <52123732+stas-job4j@users.noreply.github.com>
2019-06-30 10:35:31 +0300

    4.1. Глупый бот.[#152353]

---
+++ chapter_001/src/test/java/ru/job4j/dummybot/DummyBotTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.dummybot;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Stanislau S. ( saydobenothing@gmail.com)
+ * @version $1.0$
+ * @since 30.06.2019
+ */
+public class DummyBotTest {
+    @Test
+    public void whenGreetBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    @Test
+    public void whenByeBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+}
