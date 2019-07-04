00724990d3dba3de024d70c6e4d763bcdad989ff
stas-job4j <52123732+stas-job4j@users.noreply.github.com>
2019-07-04 21:28:51 +0300

    5.5. Рефакторинг кода.[#152361]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,22 +1,41 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
 /**
  * @author Stanislau S.
  * @version $1.0$
  * @since 04.07.2019
  */
 public class Paint {
-    /**
-     * Method pyramid.
-     * @param height .
-     * @return пирамида в псевдографике.
-     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    private String loopBy(int height, int widht, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int width = 2 * height - 1;
         for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+            for (int column = 0; column != widht; column++) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
