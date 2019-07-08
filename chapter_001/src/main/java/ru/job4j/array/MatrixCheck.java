89f2729fe3dd24d0d58f61239840f501f0cf5604
stas-job4j <saydobenothing@gmail.com>
2019-07-08 22:08:08 +0300

    6.7.1. Квадратный массив заполнен true или false по диагоналям.[#152375]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -8,27 +8,20 @@ package ru.job4j.array;
 public class MatrixCheck {
     /**
      * Method mono
+     *
      * @param data массив состоящий из boolean значений.
      * @return при равенстве элементов по диагоналям возвращает true , иначе false.
      */
     public boolean mono(boolean[][] data) {
         boolean result = true;
-        // при движении по диагонали с левого верхнего края ([0][0]) i=j , сравниваем первый элемент с остальными
-        // если они не равны , останавливаем цикл и возвращаем false
-        for (int i = 0; i < data.length; i++) {
-            if (data[0][0] != data[i][i]) {
-                result = false;
-                break;
-            }
-        }
-        // при движении по диагонали с правого верхнего края ([0][массив.длина-1] ) i уведичивается,а  j уменьшается
-        // сравниваем первый элемент с остальными ,если они не равны , останавливаем цикл и возвращаем false
-        for (int i = 0, j = data.length - 1; i < data.length; i++, j--) {
-            if (data[0][data.length - 1] != data[i][j]) {
+        int i, j;
+        for (i = 0, j = data.length - 1; i < data.length; i++, j--) {
+            if ((data[0][0] != data[i][i]) || (data[0][data.length - 1] != data[i][j])) {
                 result = false;
                 break;
             }
         }
         return result;
+
     }
 }
